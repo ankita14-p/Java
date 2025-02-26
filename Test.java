82bad7e4f32ce1f3730597a1e800a7ad4a51999f
Ankita Nag <ankeetaa3@gmail.com>
2025-02-26 17:09:44 +0530

    variable

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 class test {
-    public static void main(String args[]){
-        System.out.println("Testing");
+    public static void main(String args[])
+    {
+        int num1=3;
+        int num2=4;
+        System.out.println(num1+num2); //not a good practice
+        int sum=num1+num2;
+        System.out.println(sum); //best practice
     }
 }
